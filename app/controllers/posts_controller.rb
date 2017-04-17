24a95236c0c910124f59271f26665b86c41a460e
Nick Brierley <nickcbrierley@gmail.com>
2017-04-17 17:27:21 +0000

    checkpoint18-assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index{|post, index| post.title = "SPAM" if index % 5 == 0}
   end
 
   def show
