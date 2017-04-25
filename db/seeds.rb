5ed9f6699b94296918585b56a7e766b341e7597f
Nick Brierley <nickcbrierley@gmail.com>
2017-04-25 15:34:12 +0000

    checkpoint28 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,12 +36,20 @@ posts = Post.all
     )
 end
 
-user = User.first
-user.update_attributes!(
-    email: 'nickcbrierley@gmail.com',
-    password: 'helloworld'
-    )
+# Create an admin user
+admin = User.create!(
+    name:     'Admin User',
+    email:    'admin@example.com',
+    password: 'helloworld',
+    role:     'admin'
+)
 
+# Create a member
+member = User.create!(
+    name:     'Member User',
+    email:    'member@example.com',
+    password: 'helloworld'
+)
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
