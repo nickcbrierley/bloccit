817ad685734f2d2025e1396b8cca2804d21bc78e
Nick Brierley <nickcbrierley@gmail.com>
2017-04-25 17:31:00 +0000

    checkpoint28 assignment completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,7 +61,7 @@ class PostsController < ApplicationController
    
    def authorize_user
      post = Post.find(params[:id])
-     unless current_user == post.user || current_user.admin?
+     unless current_user == post.user || current_user.moderator? ||  current_user.admin? 
       flash[:alert] = "You need to be an admin to do that."
       redirect_to [post.topic, post]
      end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,7 @@
 class TopicsController < ApplicationController
   
   before_action :require_sign_in, except: [:index, :show]
-  before_action :authorize_user, except: [:index, :show
-  ]
+  before_action :authorize_user, except: [:index, :show]
   
    def index
      @topics = Topic.all
@@ -63,7 +62,7 @@ class TopicsController < ApplicationController
    end
    
    def authorize_user
-     unless current_user.admin?
+     unless current_user.moderator? || current_user.admin?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
      end
