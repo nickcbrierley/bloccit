4ce24a7f506af901ebbf9b5774d6b8d74d248975
Nick Brierley <nickcbrierley@gmail.com>
2017-04-25 19:36:50 +0000

    checkpoint30 assignment completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments, dependent: :destroy
     has_many :votes, dependent: :destroy
+    after_create :create_vote
     default_scope { order('rank DESC') }
     
     validates :title, length: { minimum: 5 }, presence: true
@@ -10,7 +11,7 @@ class Post < ActiveRecord::Base
     validates :topic, presence: true
     validates :user, presence: true
     
-    def up_votes
+   def up_votes
      votes.where(value: 1).count
    end
  
@@ -26,5 +27,11 @@ class Post < ActiveRecord::Base
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+   
+   private
+   
+   def create_vote
+       user.votes.create(value: 1, post: self)
    end
 end
