3a6b3e612c5ecee3536cf4f1b248d84eb5bfa390
Nick Brierley <nickcbrierley@gmail.com>
2017-04-26 17:11:44 +0000

    checkpoint31-assignment

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,5 +12,15 @@ class FavoriteMailer < ApplicationMailer
      @comment = comment
 
      mail(to: user.email, subject: "New comment on #{post.title}")
-   end
+    end
+    
+    def new_post(post)
+     headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+     headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+     headers["References"] = "<post/#{post.id}@your-app-name.example>"
+     
+     @post = post
+     
+      mail(to: post.user.email, subject: "You are now following #{post.title}")
+    end
 end
